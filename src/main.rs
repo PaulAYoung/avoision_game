da541e9f91991bb8732d91f80019de4be5853068
Paul Young <paulandresyoung@gmail.com>
2021-03-18 19:27:25 -0700

    Make a window appear

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
 
+
 fn main(){
+    App::build()
+    .add_resource(WindowDescriptor{
+        title: "Avoision".to_string(),
+        width: 500.0,
+        height: 500.0,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins).run();
 }
