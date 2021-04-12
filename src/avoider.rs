9489a377565ce598e0817f0a7934393e0ec5f138
Paul Young <paulandresyoung@gmail.com>
2021-04-11 19:23:54 -0700

    player should control avoider, not avoidee

--- src/avoider.rs
+++ src/avoider.rs
@@ -2,11 +2,11 @@ use bevy::{prelude::*, render::renderer::RenderContext};
 use crate::components::{
     Position
 };
-use crate::constants::AVOIDEE_SPEED;
+use crate::constants::AVOIDER_SPEED;
 use crate::materials::Materials;
-pub struct Avoidee;
+pub struct Avoider;
 
-pub fn spawn_avoidee(
+pub fn spawn_avoider(
     mut commands: Commands,
     materials: Res<Materials>,
 ){
@@ -19,15 +19,15 @@ pub fn spawn_avoidee(
         }
     )
     .insert(Position(Vec2::new(0.0, 0.0)))
-    .insert(Avoidee);
+    .insert(Avoider);
 }
 
 pub fn avoidee_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut avoidee_positions: Query<&mut Position, With<Avoidee>>,
+    mut avoidee_positions: Query<&mut Position, With<Avoider>>,
     time: Res<Time>
 ){
-    let move_dist = AVOIDEE_SPEED * time.delta_seconds();
+    let move_dist = AVOIDER_SPEED * time.delta_seconds();
     for mut pos in avoidee_positions.iter_mut(){
         if keyboard_input.pressed(KeyCode::Up){
             pos.0.y += move_dist;
